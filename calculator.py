01d2f7a7426dab3188652b254a68a5001e411ae8
u232777 <167290007+u232777@users.noreply.github.com>
2026-05-11 21:24:21 +0200

    .

--- calculator.py
+++ calculator.py
@@ -1,14 +1,15 @@
 def add(a, b):
-    return a + b   
+    return a + b
 
 
 def sub(a, b):
     return a - b
 
 
-def multiply(a, b): 
+def multiply(a, b):
     return a * b
 
 
-def divide(a, b): 
-    return a / b 
+def divide(a, b):
+    return a / b
+
