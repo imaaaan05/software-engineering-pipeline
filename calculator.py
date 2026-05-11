9775df787d86e980ca3d0434fa9942a503c2192b
u232777 <167290007+u232777@users.noreply.github.com>
2026-05-11 16:10:28 +0200

    Cambios en calculator.py de division

--- calculator.py
+++ calculator.py
@@ -7,3 +7,7 @@ def sub(a, b):
     
 def multiply(a, b):
     return a * b
+
+
+def divide(a, b):
+    return a / b
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -9,5 +9,9 @@ def test_sub():
     assert sub(10, 4) == 6
 
 def test_multiply():
-assert multiply(3, 4) == 12
-assert multiply(0, 5) == 0
+    assert multiply(3, 4) == 12
+    assert multiply(0, 5) == 0
+
+
+def test_divide():
+    assert divide(10, 2) == 5
