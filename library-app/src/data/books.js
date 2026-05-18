795f658dc257605ff4592f6c7b19747562f21dee
u232777 <167290007+u232777@users.noreply.github.com>
2026-05-18 15:16:07 +0200

    React library
    Initialize React library reservation app

---
+++ library-app/src/data/books.js
@@ -0,0 +1,16 @@
+const books = [
+  {
+    id: 1,
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    reserved: false,
+  },
+  {
+    id: 2,
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt",
+    reserved: false,
+  },
+]
+
+export default books
